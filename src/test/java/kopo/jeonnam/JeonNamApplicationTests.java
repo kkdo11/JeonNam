43c2e58929828813b93385640f6870f99b21a892
김도원 <kdw030612@gmail.com>
2025-06-03 22:46:15 +0900

    Initial commit

---
+++ src/test/java/kopo/jeonnam/JeonNamApplicationTests.java
@@ -0,0 +1,13 @@
+package kopo.jeonnam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JeonNamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
